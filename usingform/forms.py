a0d3ad121e2960b37d93bd797380872792bf59f2
이창우 <cwadven@naver.com>
2020-10-10 13:25:31 +0900

    날짜 정보 집어넣을 수 있도록 form 수정

--- usingform/forms.py
+++ usingform/forms.py
@@ -4,12 +4,14 @@ from .models import *
 class FormTest(forms.ModelForm):
     class Meta:
         model = Defaultform
-        fields = ('title', 'body',)
-        labels = { 'title': '제목', 'body':'내용',}
+        fields = ('title', 'body', 'start_at', 'end_at')
+        labels = { 'title': '제목', 'body':'내용', 'start_at':'시작날짜', 'end_at':'종료날짜'}
         #help_texts = { 'title': '필수 사항 입니다!', 'body':'내용을 입력해주세요!'}
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control',}),
+            'start_at': forms.DateInput(format='%Y/%m/%d', attrs={'type':'date', 'class':'form-control'}),
+            'end_at': forms.DateInput(format='%Y/%m/%d',attrs={'type':'date', 'class':'form-control'})
         }
 
 class ImageTest(forms.ModelForm):

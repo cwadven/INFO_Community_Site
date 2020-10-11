3c1aaea317121367c1cb6a97da7e925696cb8c8e
이창우 <cwadven@naver.com>
2020-10-11 11:57:16 +0900

    infoboard 빼고 다른 게시글들은 시작날짜 종료날짜 없애기

--- usingform/forms.py
+++ usingform/forms.py
@@ -14,6 +14,17 @@ class FormTest(forms.ModelForm):
             'end_at': forms.DateInput(format='%Y/%m/%d',attrs={'type':'date', 'class':'form-control'})
         }
 
+class NomalFormTest(forms.ModelForm):
+    class Meta:
+        model = Defaultform
+        fields = ('title', 'body',)
+        labels = { 'title': '제목', 'body':'내용',}
+        #help_texts = { 'title': '필수 사항 입니다!', 'body':'내용을 입력해주세요!'}
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control',}),
+        }
+
 class ImageTest(forms.ModelForm):
     class Meta:
         model = Image
